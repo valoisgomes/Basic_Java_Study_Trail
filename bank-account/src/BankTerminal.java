9e1846cc17209b41bd7e6214fc92e5820a55fff2
valoisgomes <valoisgomes84@gmail.com>
2023-07-24 14:49:34 -0300

    creating bank account challenge

---
+++ bank-account/src/BankTerminal.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class BankTerminal {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o nome do Cliente: ");
+        String clientName = scanner.next();
+
+        System.out.println("Digite o numero da agência: ");
+        String agencyNumber = scanner.next();
+
+        System.out.println("Digite o numero da conta: ");
+        int accountNumber = scanner.nextInt();
+
+        System.out.println("Digite o Saldo da Conta: ");
+        double balance = scanner.nextDouble();
+
+        System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencyNumber + 
+        " - conta: " + accountNumber + " - e seu saldo: " + balance + " - já está disponível para saque");
+
+    }
+}
