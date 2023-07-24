b8719842c7e96a3ac8e6d5ee5ae60ab893fef23e
valoisgomes <valoisgomes84@gmail.com>
2023-07-24 16:57:58 -0300

    Step flux control

---
+++ flux-control/src/ForInArray.java
@@ -0,0 +1,13 @@
+public class ForInArray {
+    
+    public static void main(String[] args) {
+        
+        String alunos [] = {"Paulo", "Pedro", "Tiago", "Lucas"};
+
+        for(int x = 0; x < alunos.length; x ++){
+            System.out.println("Aluno no index: X = " + x + " - é: " + alunos[x]);
+        }
+
+    }
+
+}
---
+++ flux-control/src/TestFor.java
@@ -0,0 +1,13 @@
+public class TestFor {
+    
+    public static void main(String[] args) {
+        
+        for(int carneiros = 1; carneiros <= 20; carneiros ++) {
+            System.out.println(carneiros + " carneirinho pulou a cerca");
+        }
+
+        System.out.println("Joãozinho Dormiu! zzzZZZzzZZzzzzZZZZ!");
+
+    }
+
+}
